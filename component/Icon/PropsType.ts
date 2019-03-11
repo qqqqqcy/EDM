8cdda939e5f914f52232606591786721879831a9
qqqqqcy <qiuchiyi@360buyad.local>
2019-03-11 18:16:00 +0800

    组件开发 | Icon

--- component/Icon/PropsType.ts
+++ component/Icon/PropsType.ts
@@ -1,3 +1,8 @@
-export interface IconProps {
-  name?: "wechat" | "alipay";
+import { DefaultBaseProps } from "../common/PropsType";
+
+export interface IconProps extends DefaultBaseProps {
+  name: "wechat" | "alipay";
+  size?: "small" | "middle" | "large";
+  onClick?: (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => void;
+  color?: string;
 }
--- component/Icon/index.tsx
+++ component/Icon/index.tsx
@@ -2,14 +2,36 @@ import * as React from "react";
 import { IconProps } from "./PropsType";
 import "./importAll";
 
-const Icon: React.FunctionComponent<IconProps> = ({ name = "wechat" }) => {
-  return (
-    <div>
-      <svg>
-        <use xlinkHref={`#${name}`} />
-      </svg>
-    </div>
-  );
+import classnames from "classnames";
+import setDefaultProps, { defaultBaseProps } from "../common/setDefaultProps";
+
+const defaultProps: GetNullableType<IconProps> = {
+  ...defaultBaseProps,
+  prefixCls: `${$PREFIX}-icon`,
+  size: "middle",
+  color: "",
+  onClick: () => {}
 };
 
-export default Icon;
+export default setDefaultProps(defaultProps, (props: Required<IconProps>) => {
+  const {
+    name,
+    size,
+    color,
+    style,
+    className,
+    prefixCls,
+    ...restProps
+  } = props;
+  const cls = classnames(prefixCls, className, `${prefixCls}-${size}`);
+  return (
+    <svg
+      aria-hidden="true"
+      className={cls}
+      style={color ? { fill: color, ...style } : style}
+      {...restProps}
+    >
+      <use xlinkHref={`#${name}`} />
+    </svg>
+  );
+});
