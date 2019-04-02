a31e60ab2bb134bd6c00362f213ae842dca149aa
qqqqqcy <qiuchiyi@360buyad.local>
2019-04-02 19:13:26 +0800

    框架搭建 | 目录结构优化，新增删除组件脚本，新增 demo 测试

---
+++ .bak/common-bak/TransitionWrap/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import classnames from 'classnames';
+import { TransitionWrapProps, TransitionStatus } from '../PropsType';
+import setDefaultProps, { transitionWrapProps } from '../setDefaultProps';
+
+interface TransitionWrapPropsWithChildren extends TransitionWrapProps {
+    children: React.ReactElement | React.ReactElement[];
+}
+
+const defaultProps: GetNullableType<TransitionWrapPropsWithChildren> = {
+    ...transitionWrapProps,
+};
+
+const statusCase: { [propName: string]: TransitionStatus } = {
+    entry: 'entry',
+    entryActive: 'entry-active',
+    entryDone: 'entry-done',
+    exit: 'exit',
+    exitActive: 'exit-active',
+    exitDone: 'exit-done',
+};
+
+export default setDefaultProps(defaultProps, (props: Required<TransitionWrapPropsWithChildren>) => {
+    const { time, visible, children, onEntry, onExitDone, onEntryDone, unmountOnExit, transitionClassName } = props;
+    const [status, setStatus]: UseType<TransitionStatus> = React.useState(visible ? statusCase.entry : statusCase.exit);
+    const [show, setShow]: UseType<boolean> = React.useState(visible);
+    const [timer, useTimer]: UseType<number> = React.useState(0);
+
+    React.useEffect(() => {
+        if (!children) {
+            return;
+        }
+        if (visible) {
+            if (status !== statusCase.entry && status !== statusCase.entryActive && status !== statusCase.entryDone) {
+                window.clearTimeout(timer);
+                setStatus(statusCase.entry);
+                if (!show && unmountOnExit) {
+                    setShow(true);
+                }
+            } else if (status === statusCase.entry) {
+                onEntry();
+                useTimer(window.setTimeout(() => setStatus(statusCase.entryActive), 0));
+            } else if (status === statusCase.entryActive) {
+                useTimer(window.setTimeout(() => setStatus(statusCase.entryDone), time));
+            } else if (status === statusCase.entryDone) {
+                onEntryDone();
+            }
+        } else {
+            if (status !== statusCase.exit && status !== statusCase.exitActive && status !== statusCase.exitDone) {
+                window.clearTimeout(timer);
+                setStatus(statusCase.exit);
+            }
+            if (status === statusCase.exit) {
+                setStatus(statusCase.exitActive);
+            } else if (status === statusCase.exitActive) {
+                useTimer(window.setTimeout(() => setStatus(statusCase.exitDone), time));
+            } else if (status === statusCase.exitDone) {
+                onExitDone();
+
+                if (show && unmountOnExit) {
+                    setShow(false);
+                }
+            }
+        }
+    }, [visible, status]);
+
+    React.useEffect(() => {
+        return () => {
+            window.clearTimeout(timer);
+            onExitDone();
+        };
+    }, []);
+
+    if (children && (show || !unmountOnExit)) {
+        return (
+            <React.Fragment>
+                {React.Children.map(children, child => {
+                    let { className } = child.props;
+                    if (transitionClassName) {
+                        className = classnames(className, `${transitionClassName}-${status}`);
+                    }
+                    return React.cloneElement(child, { className });
+                })}
+            </React.Fragment>
+        );
+    } else {
+        return null;
+    }
+});
